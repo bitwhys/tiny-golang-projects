b8b26ec6558fb14c0f2a73f6eea49649c3573798
George Bockari <bitwhys@gmail.com>
2023-06-20 15:46:51 -0400

    feat(main): enhance greet function to support English and French
    The greet function now takes a language parameter to return a greeting in the specified language. Currently, the supported languages are English and French. Tests for these new features have been added. A default case where the language is not supported has also been included, returning an empty string, with an accompanying test.

--- main_internal_test.go
+++ main_internal_test.go
@@ -2,10 +2,42 @@ package main
 
 import "testing"
 
-func TestGreet(t *testing.T) {
+func TestGreet_English(t *testing.T) {
+	// setup phase
+	lang := language("en")
 	want := "Hello world"
-	got := greet()
 
+	// execution phase
+	got := greet(lang)
+
+	//validation phase
+	if got != want {
+		// mark this test as failed
+		t.Errorf("expected: %q, got: %q", want, got)
+	}
+}
+
+func TestGreet_French(t *testing.T) {
+	lang := language("fr")
+	want := "Bonjour le monde"
+
+	got := greet(lang)
+
+	if got != want {
+		// mark this test as failed
+		t.Errorf("expected: %q, got: %q", want, got)
+	}
+}
+
+func TestGreet_Akkadian(t *testing.T) {
+	// Akkadian is not implemented yet!
+	lang := language("akk")
+	want := ""
+
+	// execution phase
+	got := greet(lang)
+
+	//validation phase
 	if got != want {
 		// mark this test as failed
 		t.Errorf("expected: %q, got: %q", want, got)
